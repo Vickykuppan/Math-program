4163e84d78a11f90d2cbcd4d24a8b9e562344d41
Vickykuppan <117091379+Vickykuppan@users.noreply.github.com>
2022-12-08 19:39:49 +0530

    Add files via upload

---
+++ Degree.java
@@ -0,0 +1,7 @@
+public class Degree
+{
+public static void main(String args[])
+{
+System.out.println(Math.sec(0));
+}
+}
---
+++ MyMath.java
@@ -0,0 +1,21 @@
+public class MyMath
+{
+public static void main(String args[])
+{
+System.out.println(Math.max(7,3));
+System.out.println(Math.min(7,3));
+System.out.println(Math.tan(90));
+System.out.println(Math.log(1010));
+System.out.println(Math.log10(1010));
+System.out.println(Math.ceil(7.6));
+System.out.println(Math.floor(7.4));
+System.out.println(Math.round(7.6));
+System.out.println(Math.random());
+System.out.println(Math.abs(-5));
+System.out.println(Math.sqrt(4));
+System.out.println(Math.cbrt(9));
+System.out.println(Math.pow(2,3));
+System.out.println(Math.sin(90));
+System.out.println(Math.cos(90));
+}
+}
